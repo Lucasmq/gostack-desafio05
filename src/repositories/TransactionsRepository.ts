eabbd68cf939be1e1be07a16a68846594587a429
Lucas Queiroz <lucasmedeirosq@gmail.com>
2020-05-23 22:27:47 -0300

    Terminado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (total, transaction) => {
+        if (transaction.type === 'income') {
+          total.income += transaction.value;
+        } else if (transaction.type === 'outcome') {
+          total.outcome += transaction.value;
+        }
+        return total;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Request): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+    const balance = this.getBalance();
+
+    if (balance.total < 0) {
+      throw Error('No money left!');
+    }
+
+    return transaction;
   }
 }
 
